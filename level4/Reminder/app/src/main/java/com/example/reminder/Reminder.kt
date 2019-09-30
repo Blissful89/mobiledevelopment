cd8e1021aec60ac715cdad98a4b953e22a55836d
Marco v. Harten <marco.van.harten.van.harten@hva.nl>
2019-09-30 10:00:23 +0200

    fix: 🐛 took wrong project

--- level4/Reminder/app/src/main/java/com/example/reminder/Reminder.kt
+++ level4/Reminder/app/src/main/java/com/example/reminder/Reminder.kt
@@ -1,5 +1,18 @@
 package com.example.reminder
 
+import android.os.Parcelable
+import androidx.room.*
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "reminderTable")
 data class Reminder(
-    var reminder: String
-)
+
+    @ColumnInfo(name = "reminder")
+    var reminder: String,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+
+) : Parcelable
