ef01342d338bfb0a3818cace308dfb2e5f5f44f8
Marco van Harten <marcovanharten89@gmail.com>
2019-12-31 15:39:52 +0100

    feat: 🎸 fake login done plus background sorted

--- final/lendahand/app/src/main/java/com/example/lendahand/MainActivity.kt
+++ final/lendahand/app/src/main/java/com/example/lendahand/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.lendahand
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+const val NAME = "test"
+const val PASSWORD = "test"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        initViews()
+    }
+
+    private fun initViews(){
+        btnLogin.setOnClickListener{fakeLogin()}
+    }
+
+    private fun fakeLogin(){
+        if(etName.text.toString() == NAME && etPassword.text.toString() == PASSWORD){
+            setContentView(R.layout.activity_home)
+        } else Toast.makeText(this, R.string.wrong_cred, Toast.LENGTH_SHORT).show()
     }
 }
