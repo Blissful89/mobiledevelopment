5724c264b07a7e9a3ecf42f2c53a49ea8e2257ce
Marco van Harten <marcovanharten89@gmail.com>
2020-01-01 12:24:42 +0100

    feat: 🎸 nav working!

--- final/lendahand/app/src/main/java/com/example/lendahand/ui/main/MainActivity.kt
+++ final/lendahand/app/src/main/java/com/example/lendahand/ui/main/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.lendahand.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.lendahand.R
+import com.example.lendahand.ui.login.LoginActivity
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawer: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +37,23 @@ class MainActivity : AppCompatActivity() {
         )
         drawer.addDrawerListener(toggle)
         toggle.syncState()
+
+        navigationView = nav_view
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_tasks -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container,TasksFragment()).commit()
+                drawer.closeDrawer(GravityCompat.START)
+            }
+            R.id.nav_logout -> {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+            else -> println("Not implemented!")
+        }
+        return true
     }
 
     override fun onBackPressed() {
